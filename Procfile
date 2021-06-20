272afa71e568173fcc93418db3646416f7fee871
mrnitric <80440353+mrnitric@users.noreply.github.com>
2021-06-20 10:24:00 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m SankiRobot
+web: python3 -m SankiRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE SankiRobot
+:: Enables virtual env mode and then starts SankiRobot
+env\scripts\activate.bat && py -m SankiRobot
